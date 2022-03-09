5603727ea80f1c6cbdc6e4bc46dc968f3ad56064
emirg1989 <eganimi1989@gmail.com>
2022-03-09 12:05:48 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # fintual-interview
+
+Para ejecutarlo solo se debe ingresar npm i y luego  npm run dev
---
+++ src/index.ts
@@ -0,0 +1,47 @@
+interface StockOptions {
+    date: Date;
+    price: number;
+}
+
+class PortfolioSimple {
+    _stocks: StockOptions[] = [];
+    constructor() {
+            this._stocks = [{
+          date: new Date("2019-09-31"),
+          price: 10
+      },
+      {
+          date: new Date("2020-09-31"),
+          price: 20
+      }];
+    }
+    //esto lo puse acá ya que me hacia más sentido que dentro de una clase Stock, ya que cada Stock debiese ser conciente de si mismo y no de los otros stocks.
+    priceByDate(date: Date) {
+      return this._stocks.find(stock => stock.date.getTime() === date.getTime()).price;
+    }
+    
+     profit(dateFrom: Date, dateTo: Date) {
+         const initialPrice = this.priceByDate(dateFrom);
+         const terminalPrice = this.priceByDate(dateTo);
+         const profit = terminalPrice - initialPrice;
+         const periodReturn = profit / initialPrice;
+         return {
+             profit,
+             annualizedReturn: this.calcAnnualizedReturn(dateFrom, dateTo, periodReturn)
+         };
+     }
+
+     calcAnnualizedReturn(dateFrom: Date, dateTo: Date, periodReturn: number) {
+        const periodInDays = this.calcPeriodInDays(dateFrom, dateTo);
+        const numbeOfYears = (365/ periodInDays);
+        return ((1 + periodReturn)**(numbeOfYears) - 1).toFixed(5);
+     }
+
+     calcPeriodInDays(dateFrom: Date, dateTo: Date){
+        const diff = dateTo.getTime() - dateFrom.getTime();
+        return Math.round(diff / (1000 * 60 * 60 * 24));
+     }
+  }
+  
+  const portfolio = new PortfolioSimple();
+  console.log(portfolio.profit(new Date("2019-09-31"), new Date("2020-09-31")));

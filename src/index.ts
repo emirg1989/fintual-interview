2174cd05245d2166f722244981ab455200b0f559
emirg1989 <eganimi1989@gmail.com>
2022-03-10 12:25:45 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,11 @@ interface StockOptions {
     price: number;
 }
 
+interface ProfitObject {
+    profit: number;
+    annualizedReturn: string;
+}
+
 class PortfolioSimple {
     _stocks: StockOptions[] = [];
     constructor() {
@@ -16,11 +21,11 @@ class PortfolioSimple {
       }];
     }
     //esto lo puse acá ya que me hacia más sentido que dentro de una clase Stock, ya que cada Stock debiese ser conciente de si mismo y no de los otros stocks.
-    priceByDate(date: Date) {
+    priceByDate(date: Date): number {
       return this._stocks.find(stock => stock.date.getTime() === date.getTime()).price;
     }
     
-     profit(dateFrom: Date, dateTo: Date) {
+     profit(dateFrom: Date, dateTo: Date): ProfitObject {
          const initialPrice = this.priceByDate(dateFrom);
          const terminalPrice = this.priceByDate(dateTo);
          const profit = terminalPrice - initialPrice;
@@ -31,13 +36,13 @@ class PortfolioSimple {
          };
      }
 
-     calcAnnualizedReturn(dateFrom: Date, dateTo: Date, periodReturn: number) {
+     calcAnnualizedReturn(dateFrom: Date, dateTo: Date, periodReturn: number): string {
         const periodInDays = this.calcPeriodInDays(dateFrom, dateTo);
         const numbeOfYears = (365/ periodInDays);
         return ((1 + periodReturn)**(numbeOfYears) - 1).toFixed(5);
      }
 
-     calcPeriodInDays(dateFrom: Date, dateTo: Date){
+     calcPeriodInDays(dateFrom: Date, dateTo: Date): number{
         const diff = dateTo.getTime() - dateFrom.getTime();
         return Math.round(diff / (1000 * 60 * 60 * 24));
      }
